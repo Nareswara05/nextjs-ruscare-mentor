b7d83c89f72a1dfe85216ae300d05f731df3ec2c
Nareswara05 <naresalhaq9@gmail.com>
2024-09-02 14:42:37 +0700

    feat [NR] fix data mentor

--- src/app/lib/service/endpoint/mentor/get-mentor.js
+++ src/app/lib/service/endpoint/mentor/get-mentor.js
@@ -3,7 +3,7 @@ import instance from "../../instance/instance";
 export default async function getMentor() {
   try {
     const response = await instance.get('/mentor/detail');
-    return response.data;
+    return response.data.data;
   } catch (error) {
     console.error("Terjadi kesalahan saat mengambil data pengguna:", error);
     throw error;
